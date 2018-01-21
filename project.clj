5e7c86918550f984ef900c5b8a5df7880e6409f9
Alex Gunnarson <alexandergunnarson@gmail.com>
2018-01-20 21:04:36 -0700

    quantum/re-posh 0.1.6

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject re-posh "0.1.5"
+(defproject quantum/re-posh "0.1.6"
   :description "Use your re-frame with DataScript as a data storage"
   :url "https://github.com/denistakeda/re-posh"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [re-frame "0.9.4"]
-                 [posh "0.5.5"]])
+                 [quantum/posh "0.5.7"]])
